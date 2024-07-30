516efa72fb35a61b2506ca1fdecb5cf0e6095b37
EMMANUEL <waruterewachira7@gmail.com>
2024-07-30 22:55:51 +0300

    update

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,39 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    # takes a list of spicy_foods and returns a list of strings with the names of each spicy food.
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    # takes a list of spicy_foods and returns a list of dictionaries where the heat level of the food is greater than 5.
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    # takes a list of spicy_foods and outputs to the terminal each spicy food in the following format using print(): Buffalo Wings (American) | Heat Level: 🌶🌶🌶
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    # takes a list of spicy_foods and a string representing a cuisine, and returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    # takes a list of spicy_foods and outputs to the terminal ONLY the spicy foods that have a heat level greater than 5
+    for food in get_spiciest_foods(spicy_foods):
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
+    
 
 def get_average_heat_level(spicy_foods):
-    pass
+    # takes a list of spicy_foods and returns the average heat level of all the foods in the list.
+    # Recall that to derive the average of a collection, you need to calculate the total and divide number of elements in the collection.
+    return sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    # takes a list of spicy_foods and a dictionary representing a new spicy food, and returns the updated list with the new spicy food added.
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+    
